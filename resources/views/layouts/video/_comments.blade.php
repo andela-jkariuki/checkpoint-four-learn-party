50af291a253f5ec7e16cd1977f2b79f3f9490293
John Kariuki <john.kariuki@andela.com>
2016-03-15 20:19:26 +0300

    [feature #115691805] Update Comments view

--- resources/views/layouts/video/_comments.blade.php
+++ resources/views/layouts/video/_comments.blade.php
@@ -1,6 +1,6 @@
 
 <div class="row">
-    <ul class="nav nav-tabs" role="tablist">
+    <ul class="nav nav-tabs" role="tablist" id="comment-section">
         <li class="active"><a href="#all-comments" role="tab" data-toggle="tab">Comments</a></li>
         <li><a href="#new-comment" role="tab" data-toggle="tab">Add comment</a></li>
     </ul>
@@ -38,15 +38,72 @@
             </ul>
         </div>
         <div class="tab-pane" id="new-comment">
-            <form action="#" method="post" class="form-horizontal" id="commentForm" role="form">
-                <div class="form-group">
-                    <label for="email" class="control-label">Comment</label>
-                    <textarea class="form-control" name="addComment" id="addComment" rows="5"></textarea>
+            <div id="feedback">
+                <div class="alert alert-danger alert-dismissible" role="alert" style="display: none;">
+                  <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                  Error. No comment provided or error processing request. Please try again.
                 </div>
-                <div class="form-group">
-                      <button class="btn btn-success" type="submit" id="submitComment"><span class="fa fa-comment"></span> Summit comment</button>
+            </div>
+                {!! Form::open([
+                        'method' => 'POST',
+                        'action' => ['CommentController@create'],
+                        'class' => 'form'
+                    ])
+                !!}
+
+                <div class="form-group{{ $errors->has('comment') ? ' has-error' : '' }}">
+                    {!! Form::label('comment', 'Comment') !!}
+                    {!! Form::textarea('comment', null, ['class' => 'form-control', 'id' => 'comment']) !!}
+                    @if ($errors->has('comment'))
+                        <span class="help-block">
+                            <strong>{{ $errors->first('comment') }}</strong>
+                        </span>
+                    @endif
                 </div>
-            </form>
+
+                <div class="form-group">
+                    <button type="button" id="newComment" class="btn btn-success">
+                        <i class="fa fa-btn comment"></i> Add comment
+                    </button>
+                </div>
+
+            {!! Form::close() !!}
         </div>
     </div>
 </div>
+
+@section('js')
+    <script type="text/javascript">
+        $(document).ready(function ()
+        {
+            $('#newComment').on("click", function ()
+            {
+                if ($('#comment').val().trim().length !== 0) {
+                    $addNewComment = $.ajax({
+                        type : 'POST',
+                        url: '{{ route("new_comment") }}',
+                        data: {
+                            comment:  $('#comment').val().trim(),
+                            video_id: {{ (int) $video->id }}
+                        }
+                    });
+
+                    $addNewComment.done(function (response) {
+                        $('#feedback div').hide();
+                        $("#comment-section li a:first").tab('show');
+                    });
+
+                    $addNewComment.fail(function (response) {
+                        $('#feedback div').show();
+                    });
+                } else{
+                    $('#feedback div').show();
+                }
+            });
+        });
+      </script>
+    <!-- @if ($errors->has('comment'))
+            $('#comment-section li a:last').tab('show');
+            $("html, body").animate({ scrollTop: $(document).height() }, 1000);
+    @endif -->
+@endsection
