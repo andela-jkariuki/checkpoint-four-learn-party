734f385f2f338dbdf7cdbdfc636e0cdbbd68ae93
John Kariuki <john.kariuki@andela.com>
2016-03-17 20:30:33 +0300

    [feature 115737819] Refeactor  seeder and factory classes

--- database/seeds/VideosTableSeeder.php
+++ database/seeds/VideosTableSeeder.php
@@ -29,57 +29,46 @@ class VideosTableSeeder extends Seeder
     {
         factory(LearnParty\Video::class)->create([
             'url' => 'm1_EDno-44M',
-            'views' => rand(1, 100)
         ]);
 
         factory(LearnParty\Video::class)->create([
             'url' => 'pLs4Tex0U1U',
-            'views' => rand(1, 100)
         ]);
 
         factory(LearnParty\Video::class)->create([
             'url' => 'UDwe95oocsw',
-            'views' => rand(1, 100)
         ]);
 
         factory(LearnParty\Video::class)->create([
             'url' => '3Hn9hLOljJI',
-            'views' => rand(1, 100)
         ]);
 
         factory(LearnParty\Video::class)->create([
             'url' => 'LrCHz1gwzTo',
-            'views' => rand(1, 100)
         ]);
 
         factory(LearnParty\Video::class)->create([
             'url' => 'JQbiR2gtyw0',
-            'views' => rand(1, 100)
         ]);
 
         factory(LearnParty\Video::class)->create([
             'url' => 'H_AQFnqMY3E',
-            'views' => rand(1, 100)
         ]);
 
         factory(LearnParty\Video::class)->create([
             'url' => 'ttWobUlBFOY',
-            'views' => rand(1, 100)
         ]);
 
         factory(LearnParty\Video::class)->create([
             'url' => 'ReI6gvzVP0Y',
-            'views' => rand(1, 100)
         ]);
 
         factory(LearnParty\Video::class)->create([
             'url' => 'UDwe95oocsw',
-            'views' => rand(1, 100)
         ]);
 
         factory(LearnParty\Video::class, 40)->create([
             'url' => 'k8tzfpeyPzw',
-            'views' => rand(1, 100)
         ]);
     }
 }
