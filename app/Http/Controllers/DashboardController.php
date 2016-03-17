d4269a17793d931f77e7e8c5f59fc28ea5ce3fe5
John Kariuki <john.kariuki@andela.com>
2016-03-17 23:34:08 +0300

    [Fix 115892133] Refractor routes and middleware usage from controllers to routes file

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -14,7 +14,6 @@ class DashboardController extends Controller
 {
     public function __construct(VideoRepository $videoRepository)
     {
-        $this->middleware('auth');
         $this->videoRepository = $videoRepository;
     }
 
