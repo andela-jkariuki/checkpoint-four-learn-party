390ce5cb8ca68004fc484dd7803dc1cc9d8906c0
John Kariuki <john.kariuki@andela.com>
2016-03-17 20:11:13 +0300

    [feature 115864741] Add side nav popular videos by comments, likes and favorites

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -5,57 +5,9 @@
 <div class="container-fluid">
     <div class="row">
         <div class="col-md-3">
-            <div class="col-md-12">
-                <div class="panel panel-default">
-                    <div class="panel-heading">
-                        <h3 class="panel-title">Videos By Category</h3>
-                    </div>
-                    <div class="panel-body popular-videos">
-                        <span class="badge"><a href="#">All</a></span>
-                        <span class="badge"><a href="#">javascript</a></span>
-                        <span class="badge"><a href="#">php</a></span>
-                        <span class="badge"><a href="#">java</a></span>
-                        <span class="badge"><a href="#">python</a></span>
-                        <span class="badge"><a href="#">ruby</a></span>
-                    </div>
-                </div>
-            </div>
-            <div class="col-md-12">
-                <div class="panel panel-default">
-                    <div class="panel-heading">
-                        <h3 class="panel-title">Popular - Most Viewed</h3>
-                    </div>
-                    <div class="panel-body popular-videos">
-                        <h5 class="top-videos"><i class="fa fa-youtube-play"></i> <a href="#">A very cool ass title</a></h5>
-                        <h5 class="top-videos"><i class="fa fa-youtube-play"></i> <a href="#">A very cool ass title</a></h5>
-                        <h5 class="top-videos"><i class="fa fa-youtube-play"></i> <a href="#">A very cool ass title</a></h5>
-                    </div>
-                </div>
-            </div>
-            <div class="col-md-12">
-                <div class="panel panel-default">
-                    <div class="panel-heading">
-                        <h3 class="panel-title">Popular - Most Liked</h3>
-                    </div>
-                    <div class="panel-body popular-videos">
-                        <h5 class="top-videos"><i class="fa fa-youtube-play"></i> <a href="#">A very cool ass title</a></h5>
-                        <h5 class="top-videos"><i class="fa fa-youtube-play"></i> <a href="#">A very cool ass title</a></h5>
-                        <h5 class="top-videos"><i class="fa fa-youtube-play"></i> <a href="#">A very cool ass title</a></h5>
-                    </div>
-                </div>
-            </div>
-            <div class="col-md-12">
-                <div class="panel panel-default">
-                    <div class="panel-heading">
-                        <h3 class="panel-title">Popular - Most commented on</h3>
-                    </div>
-                    <div class="panel-body popular-videos">
-                        <h5 class="top-videos"><i class="fa fa-youtube-play"></i> <a href="#">A very cool ass title</a></h5>
-                        <h5 class="top-videos"><i class="fa fa-youtube-play"></i> <a href="#">A very cool ass title</a></h5>
-                        <h5 class="top-videos"><i class="fa fa-youtube-play"></i> <a href="#">A very cool ass title</a></h5>
-                    </div>
-                </div>
-            </div>
+
+            @include('layouts.side_nav')
+
         </div>
         <div class="col-md-9">
             <div id="video-library">
@@ -67,7 +19,7 @@
                     <div class="row">
                     @foreach($chunk as $video)
 
-                        @include ('layouts._video')
+                        @include ('layouts..video.video_card')
 
                     @endforeach
 
