733970840dd04cf94de9f90ba91df64892ab3a90
John Kariuki <john.kariuki@andela.com>
2016-03-19 21:23:59 +0300

    [chore 115985449] Assert that getLikeStatus returns false for an unauthenticated user

--- tests/VideoTest.php
+++ tests/VideoTest.php
@@ -8,6 +8,8 @@ use LearnParty\Http\Repositories\VideoRepository;
 class VideoTest extends TestCase
 {
     /**
+     * VideoRepository getVideo test
+     *
      * Test get a single Video
      *
      * @return void
@@ -23,6 +25,8 @@ class VideoTest extends TestCase
     }
 
     /**
+     * VideoRepository getAllComments test
+     *
      * Test that a user viewing a video is able to get all the
      * comments on a video
      *
@@ -43,6 +47,8 @@ class VideoTest extends TestCase
     }
 
     /**
+     *  VideoRepository makeYoutubeUrl tests
+     *
      * Test that the make Youtube Url correctly parses a youtube Url
      */
 
@@ -55,6 +61,8 @@ class VideoTest extends TestCase
     }
 
     /**
+     * VideoRepository updateViews test
+     *
      * Test that when a user loads a video, they update the view on a video
      *
      * @return
@@ -67,5 +75,24 @@ class VideoTest extends TestCase
         $this->videoRepository->updateViews($video->id);
 
         $this->assertEquals(6, $this->videoRepository->getVideo($video->id)->views);
+    }
+
+    /**
+     * VideoRepository getLikeStatus
+     *
+     * Assert that the getLike status ..
+     *
+     *  returns false if not loggged in
+     *  returns false if logged in and not like a video
+     *  returns true if logged in and user has liked a video
+     *
+     * @return [type] [description]
+     */
+    public function testGetLikeStatus()
+    {
+        $user = factory('LearnParty\User')->create();
+        $video = factory('LearnParty\Video')->create();
+
+        $this->assertFalse($this->videoRepository->getLikeStatus($video));
     }
 }
