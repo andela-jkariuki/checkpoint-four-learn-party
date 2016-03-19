ae36169ca4c4ee0fcb6d149a0e5fd5f0049f6125
John Kariuki <john.kariuki@andela.com>
2016-03-20 01:53:06 +0300

    [Chore 115991437] Assert that users can see all the videos uploaded by a specific user

--- tests/UsersTest.php
+++ tests/UsersTest.php
@@ -9,7 +9,7 @@ class UsersTest extends TestCase
     use  LearnParty\Tests\PersistTestData;
 
     /**
-     * Assert that a user is able to view their profile
+     * Assert that a user is able to view their profile.
      *
      * @return void
      */
@@ -23,16 +23,39 @@ class UsersTest extends TestCase
     }
 
     /**
-     * Assert that a user can update their profile
+     * Assert that a user can update their profile.
      *
      * @return void
      */
     public function testUserCanUpdateProfile()
     {
         $user = factory('LearnParty\User')->create();
-        $x = $this->actingAs($user)
+        $this->actingAs($user)
              ->visit('/profile')
              ->type('A swanky new profile update', 'about')
              ->press('update-profile');
+    }
+
+    /**
+     * Assert that users can see all the videos uploaded by a specific
+     * user
+     *
+     * @return void
+     */
+    public function testUsersCanSeeVideosByAUser()
+    {
+        $user = factory('LearnParty\User')->create();
+        $videos = factory('LearnParty\Video', 3)->create(['user_id' => 1]);
+
+        $this->visit('users/' .$user->id)
+             ->see($videos[0]->name)
+             ->see($videos[1]->name)
+             ->see($videos[2]->name);
+
+        $this->call(
+            'GET',
+            'users/' . $user->id
+        );
+        $this->assertViewHasAll(['user', 'videos', 'headline']);
     }
 }
