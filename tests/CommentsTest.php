c0b4cd9a804bc9fc0b2ae6c0c5bb992d2c64f2d3
John Kariuki <john.kariuki@andela.com>
2016-03-20 14:09:09 +0300

    [Chore 115993955] Test user-comment, video-comment relationship

--- tests/CommentsTest.php
+++ tests/CommentsTest.php
@@ -29,4 +29,44 @@ class CommentsTest extends TestCase
         $this->assertTrue(is_array($feedback->original));
         $this->assertEquals('New comment succesfully added.', $feedback->original['message']);
     }
+
+    /**
+     * Assert that a comment belongs to a user
+     *
+     * @return void
+     */
+    public function testUserCommentRelationship()
+    {
+        $user = $this->createAndLoginUser();
+        $video = factory('LearnParty\Video')->create();
+
+        $this->actingAs($user)
+             ->call('POST', 'comments/create', [
+                    '_token' => csrf_token(),
+                    'comment' => 'A swanky new comment',
+                    'video_id' => $video->id
+             ]);
+
+        $this->assertEquals(LearnParty\Comment::first()->user->id, $user->id);
+    }
+
+    /**
+     * Assert that a comment belongs to a Video
+     *
+     * @return void
+     */
+    public function testVideoCommentRelationship()
+    {
+        $user = $this->createAndLoginUser();
+        $video = factory('LearnParty\Video')->create();
+
+        $this->actingAs($user)
+             ->call('POST', 'comments/create', [
+                    '_token' => csrf_token(),
+                    'comment' => 'A swanky new comment',
+                    'video_id' => $video->id
+             ]);
+
+        $this->assertEquals(LearnParty\Comment::first()->video->id, $video->id);
+    }
 }
